2822fc6bdc213ee8a65f33d1e72e5da6b38c9c5f
dvnchu <dculini@gmail.com>
2026-05-10 00:31:08 -0300

    chat working

--- testct1.py
+++ testct1.py
@@ -8,14 +8,14 @@ DESTINATARY = input("Who are you messaging: ")
 
 def sendmsg(serverSck):
     while True:
-        data = input("whats the message ")
+        data = input()
         if data == "exit": break
         serverSck.sendall(data.encode('utf-8'))
 
 def receivemsg(serverSck):
     while True:
         msg = serverSck.recv(1024)
-        print(msg.decode('utf-8'))
+        print("\n", f"{USER}:{msg.decode('utf-8')}")
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sck:
     sck.connect((HOST,PORT))
