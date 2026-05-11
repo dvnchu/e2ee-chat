852786533627bcfaf9bdcae26fae5100f9287a18
dvnchu <dculini@gmail.com>
2026-05-11 20:42:49 -0300

    started migrating msg to JSON

--- testct1.py
+++ testct1.py
@@ -1,16 +1,36 @@
 import socket
 import threading
+import json
+from datetime import datetime, timezone
 
 HOST = '127.0.0.1'
 PORT = 50007
-USER = input("Who are you:")
-DESTINATARY = input("Who are you messaging: ")
+USER = input("Register with email:")
+
+def createPack(msg_type, sender, target=None, message=None):
+    pack = {
+        "type": msg_type,
+        "sender": sender,
+        "timestamp": datetime.now(timezone.utc).isoformat()
+    }
+    if msg_type == "login":
+        pack["content"] = "sync_request"
+    elif msg_type == "chat_msg":
+        if not target or not message:
+            raise ValueError("chat_msg requires target y message")
+        pack["target"] = target
+        pack["content"] = message
+    else:
+        return b"" 
+    return json.dumps(pack).encode("utf-8")
+
 
 def sendmsg(serverSck):
     while True:
-        data = input()
+        data = input().split(':')
+        message = createPack("chat_msg", USER, data[0], data[1])
         if data == "exit": break
-        serverSck.sendall(data.encode('utf-8'))
+        serverSck.sendall(message)
 
 def receivemsg(serverSck):
     while True:
@@ -19,8 +39,7 @@ def receivemsg(serverSck):
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sck:
     sck.connect((HOST,PORT))
-    chat = f"{USER}|{DESTINATARY}"
-    sck.sendall(chat.encode('utf-8'))
+    sck.sendall(USER.encode('utf-8'))
     threading.Thread(target = receivemsg, args=(sck,)).start()
     sendmsg(sck)
 
--- testsv.py
+++ testsv.py
@@ -1,5 +1,7 @@
 import socket
 import threading
+import json
+from datetime import datetime, timezone
 
 HOST = ''
 PORT = 50007
@@ -7,24 +9,61 @@ PORT = 50007
 users = {}
 users_lock = threading.Lock()
 
+
+def parse_json(data):
+    try:
+        data_string = data.decode("utf-8")
+        pack = json.loads(data_string)
+
+        return pack
+    except json.JSONDecodeError:
+        print("Error: El paquete recibido no es un JSON válido")
+        return None
+    except UnicodeDecodeError:
+        print("Error: No se pudo decodificar el formato de texto")
+
+
+def createPack(msg_type, sender, target=None, message=None):
+    pack = {
+        "type": msg_type,
+        "sender": sender,
+        "timestamp": datetime.now(timezone.utc).isoformat()
+    }
+    if msg_type == "login":
+        pack["content"] = "sync_request"
+    elif msg_type == "chat_msg":
+        if not target or not message:
+            raise ValueError("chat_msg requires target y message")
+        pack["target"] = target
+        pack["content"] = message
+    else:
+        return b"" 
+    return json.dumps(pack).encode("utf-8")
+
+
+
+
 def handler(clientSck):
-        chat = clientSck.recv(1024).decode('utf-8')
-        parts = chat.split('|')
-        sender = parts[0]
-        receiver = parts[1]
+        user_sync = clientSck.recv(1024).decode('utf-8')
+        pack_sync = parse_json(user_sync)
         with users_lock:
-            users[sender] = clientSck
-            print(f"Usuario {sender} conectado.")
+            if pack_sync and pack_sync["sender"] in users:
+                user = pack_sync["sender"]
+                print(f"Usuario {user} conectado.")
+            else:
+                clientSck.
         try:
             while True:
-                data = clientSck.recv(1024)
+                data = clientSck.recv(1024).decode()
+                json.d
                 if not data: break
                 users[receiver].send(data)
         finally:
             with users_lock:
-                if sender in users:
-                    del users[sender]
-                print(f"Usuario {sender} desconectado")
+                if user in users:
+                    del users[user]
+                print(f"Usuario {user} desconectado")
+
 
 
 with socket.socket(family = socket.AF_INET, type=socket.SOCK_STREAM) as listeningSck:
